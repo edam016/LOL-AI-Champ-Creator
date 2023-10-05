502dfcf1e6ab728c1211b22573f1cc8a8abc8632
Eric Dam <edam3268@gmail.com>
2023-10-05 17:29:45 -0400

    Added Hero button on homepage

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -7,16 +7,15 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <img src="lol-logo.png" alt="League of Legends Logo" />
+        {/* <img src="lol-logo.png" alt="League of Legends Logo" /> */}
       </div>
-      <div className="nav-links">
+      {/* <div className="nav-links">
         <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">Champions</a></li>
-          <li><a href="#">Items</a></li>
-          <li><a href="#">News</a></li>
+          <li><a href="#">Create</a></li>
         </ul>
-      </div>
+      </div> */}
       <div className="champ-creator">League of Legends Champ Creator</div>
     </nav>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 import EntryList from './EntryList';
 import './page.css';
 import Navbar from './Navbar';
+import ChampCreation from './ChampCreation';
+import BigButtonComponent from './BigButtonComponent';
 
 function Page() {
   return (
     <div className="App">
       <Navbar />
-      <EntryList />
+      <BigButtonComponent />
     </div>
   );
 }
