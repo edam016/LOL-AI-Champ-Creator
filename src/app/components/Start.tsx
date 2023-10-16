8755cbae5d9342dc0fa51b57b64bd07355f774a9
Eric Dam <edam3268@gmail.com>
2023-10-16 15:00:35 -0400

    Removed border and fixed API key

--- src/app/components/Start.tsx
+++ src/app/components/Start.tsx
@@ -34,12 +34,15 @@ const Start = () => {
         <br/>
         Greetings, fellow summoners! I'm excited to introduce the new <u><b>AI League of Legends Champion Concept Creator</b></u>. Get ready to embark on a journey of creativity and innovation as you shape your very own champion for the Rift!<br/><br/>
         📜 Patch Highlights:<br/>
-        - Dive into the creative process with a series of questions.<br/>
-        <u><b> - Only the first prompt, which includes tags, is mandatory. The AI will help generate content for the remaining prompts, leaving the creative canvas open for your genius.</b></u><br/>
+        - Dive into the creative process of champion creation using the power of AI
         <br/>
+        - Add tags related to your champion with your imagination being the limit
+        <br/>
+        <br />
         📖 Patch Notes:
         <br />
-        As a dedicated fan of League of Legends and technology, I'm thrilled to bring you this new tool. It's designed to help you craft your dream champion and breathe life into your unique ideas. You'll start with the first prompt, which focuses on tags, and from there, the possibilities are endless. Feel free to explore and unleash your imagination as you design your champion's lore, abilities, and more!
+        As a dedicated fan of League of Legends and technology, I'm thrilled to bring you this new tool. It's designed to help you craft your dream champion and breathe life into your unique ideas. 
+        You'll start with a prompt, which asks for tags related to your champion, and with the power of AI using OpenAI's API, the possibilities are endless!<br/> Feel free to explore and unleash your imagination as you design your champion's lore, abilities, and more!
         <br /><br/>
         🎮 Ready to embark on your creative journey? Let's dive into the world of champion creation and make your mark on the Summoner's Rift. Happy designing!
         <br /><br/>
